3942de2f337011576f28eaddfb375cb23bab9f06
l3b0n <ndabashinzes@gmail.com>
2021-03-01 10:21:53 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+ const btnOpen = document.querySelector("#menu")
+ const btnClose = document.querySelector("#menu-close")
+ const menu = document.querySelector(".menu")
+ btnOpen.addEventListener('click',()=>{
+     menu.style.left = 0
+ })
+ btnClose.addEventListener('click',()=>{
+    menu.style.left = "-1000px"
+})
+
+// testing
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+// var bloc = document.querySelector(".menu-parent")
+//     // console.log(bloc)
